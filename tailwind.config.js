e8936ae76e0432641405b79ddc96b4b63d852eb1
zaypugh29 <123493030+zaypugh29@users.noreply.github.com>
2023-12-12 21:50:43 -0500

    start using nike clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./layouts/**/*.html", "./content/**/*.html", "./content/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nike-tan': '#f5f5f5',
+        'nike-hover-link': '#757575',
+      }
+    },
   },
   plugins: [],
 }
