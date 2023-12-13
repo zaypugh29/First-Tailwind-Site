6d6b069d75aa0f7631f0683e37317c5c01a9cb5d
zaypugh29 <123493030+zaypugh29@users.noreply.github.com>
2023-12-12 19:37:30 -0500

    add stuff

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.html", "./content/**/*.html", "./content/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
