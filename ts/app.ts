49c956fa5d732a6bf14fe1d3760d0ba423a9eb0e
zaypugh29 <123493030+zaypugh29@users.noreply.github.com>
2023-12-15 19:38:11 -0500

    typescript

---
+++ ts/app.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+interface Starwars {
+    name?: string, 
+    height?: number,
+    mass?: number,
+    skin_color?: string,
+    hair_color?: string,
+    eye_color?: string,
+    birth_year?: number,
+    gender?: string,
+    homeword?: string,
+    films?: string[],
+    species?: string[],
+    vehicles?: string[],
+    starships?: string[],
+    create?: string,
+    edited?: string,
+    url?: string
+}
+const getInfo = async () => {
+    const resp = (await axios.get<Starwars>('https://swapi.dev/api/people')).data;
+    console.log(resp);
+} 
+
+// getInfo();
